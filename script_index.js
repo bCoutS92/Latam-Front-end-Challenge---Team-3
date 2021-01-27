c2419229ceb9340e715e048f6732d598b56c746d
bcoutinho-xandr <62029601+bcoutinho-xandr@users.noreply.github.com>
2021-01-27 18:25:31 -0300

    Update script_index.js

--- script_index.js
+++ script_index.js
@@ -1,1 +1,25 @@
+var target = document.querySelector('#new_asts');
 
+document.addEventListener('scroll', () => {
+  if (window.scrollY >= target.getBoundingClientRect().top) {
+    console.log('Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam tincidunt massa eros, eget efficitur quam ultrices lacinia. Curabitur condimentum dui eu ligula hendrerit, sed imperdiet ipsum dignissim. Suspendisse id gravida tortor. Nunc vestibulum tincidunt lacinia. Pellentesque sagittis turpis at sapien cursus lobortis. Sed bibendum, ex non sagittis ultrices, turpis diam tincidunt eros, ac auctor ante metus sed nisl. Cras cursus lectus risus, vitae ultricies nulla varius in. Curabitur odio enim, mattis sed feugiat nec, lacinia vel urna. Donec imperdiet dapibus auctor. Nullam id mauris fermentum, dapibus tortor rhoncus, tempus augue. Ut consequat magna justo, sed lacinia sapien egestas eu. Fusce felis erat, sollicitudin non egestas nec, maximus faucibus neque. Proin a ipsum et felis iaculis dignissim eu id nisl.
+
+Donec id ante et mi tristique consequat. Praesent sollicitudin suscipit purus a cursus. Vestibulum in dolor lacus. Duis accumsan ante sit amet erat commodo ornare quis id sapien. Integer tristique risus ut interdum ultrices. Sed in ultricies est. Curabitur vehicula lectus nibh, non facilisis orci blandit at. Donec placerat tincidunt leo ac rhoncus. Cras varius ante quis urna scelerisque sodales. Donec quam ante, gravida quis metus et, vehicula tempus odio. Donec efficitur erat nulla. Vestibulum eu libero luctus felis tempor molestie. Nullam pharetra pulvinar erat fringilla tempus.');
+  }
+})
+
+var target2 = document.querySelector('#tag_content');
+
+document.addEventListener('scroll', () => {
+  if (window.scrollY >= target.getBoundingClientRect().top) {
+    console.log('Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam tincidunt massa eros, eget efficitur quam ultrices lacinia. Curabitur condimentum dui eu ligula hendrerit, sed imperdiet ipsum dignissim. Suspendisse id gravida tortor. Nunc vestibulum tincidunt lacinia. Pellentesque sagittis turpis at sapien cursus lobortis. Sed bibendum, ex non sagittis ultrices, turpis diam tincidunt eros, ac auctor ante metus sed nisl. Cras cursus lectus risus, vitae ultricies nulla varius in. Curabitur odio enim, mattis sed feugiat nec, lacinia vel urna. Donec imperdiet dapibus auctor. Nullam id mauris fermentum, dapibus tortor rhoncus, tempus augue. Ut consequat magna justo, sed lacinia sapien egestas eu. Fusce felis erat, sollicitudin non egestas nec, maximus faucibus neque. Proin a ipsum et felis iaculis dignissim eu id nisl.
+
+Donec id ante et mi tristique consequat. Praesent sollicitudin suscipit purus a cursus. Vestibulum in dolor lacus. Duis accumsan ante sit amet erat commodo ornare quis id sapien. Integer tristique risus ut interdum ultrices. Sed in ultricies est. Curabitur vehicula lectus nibh, non facilisis orci blandit at. Donec placerat tincidunt leo ac rhoncus. Cras varius ante quis urna scelerisque sodales. Donec quam ante, gravida quis metus et, vehicula tempus odio. Donec efficitur erat nulla. Vestibulum eu libero luctus felis tempor molestie. Nullam pharetra pulvinar erat fringilla tempus.
+
+In sed nibh ac diam feugiat placerat. Nulla at lorem at urna malesuada rhoncus ac feugiat velit. Nam pulvinar, est nec cursus ornare, nunc nisi sodales nisl, id faucibus quam sapien nec nibh. Quisque mollis, est quis consequat dapibus, eros magna aliquam enim, et dictum augue felis et urna. Praesent tincidunt luctus scelerisque. Vestibulum in ullamcorper enim. Etiam nec lacinia est. Nulla sed suscipit ex, eget ornare lacus. Phasellus lorem diam, interdum non elit at, consequat euismod erat. Etiam cursus euismod rhoncus. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Sed in interdum magna. Pellentesque dictum mi et facilisis mollis.
+
+Sed volutpat eros tincidunt, cursus ipsum in, bibendum lacus. Sed fringilla sem a sem vulputate iaculis. Etiam interdum placerat ante, ac mattis lacus finibus vel. Sed neque tortor, luctus nec cursus sit amet, eleifend vitae lacus. Aenean eget pulvinar neque. Sed interdum augue et dolor consequat eleifend. Proin tellus orci, lacinia at gravida non, suscipit eu metus. Integer fermentum lorem non metus congue sodales. Nullam aliquam lacinia fringilla. Nunc at pulvinar purus, in sollicitudin urna. Nunc sollicitudin ligula vel suscipit tempor. Pellentesque sed ante at elit molestie porta sit amet a ligula. Nulla facilisi.
+
+Praesent venenatis accumsan augue. Aliquam pretium, purus non sagittis ultricies, nunc nisl laoreet leo, eu rutrum dui felis vitae diam. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed ornare turpis vitae magna eleifend suscipit. Fusce nec diam volutpat, faucibus odio convallis, placerat velit. Nunc pulvinar malesuada tristique. Fusce iaculis nulla dui, accumsan semper nisi tristique eu. Vivamus consequat ac turpis et fringilla. Integer sit amet luctus ligula. Quisque sit amet leo porta, finibus elit id, consequat orci. ');
+  }
+})
